44e0364524d2ef278c3faa760d757dc9bfc1292d
Rizart Dokollari <r.dokollari@gmail.com>
2021-05-31 10:24:51 +0100

    Remove optional private field.

--- s3.tf
+++ s3.tf
@@ -1,23 +1,19 @@
 resource "aws_s3_bucket" "test-cicd-us-east-1" {
   bucket   = "${var.org}-test-cicd-${var.us_east_1}"
-  acl      = "private"
   provider = aws.us_east_1
 }
 
 resource "aws_s3_bucket" "prod-cicd-us-east-1" {
   bucket   = "${var.org}-prod-cicd-${var.us_east_1}"
-  acl      = "private"
   provider = aws.us_east_1
 }
 
 resource "aws_s3_bucket" "test-cicd-eu-west-1" {
   bucket = "${var.org}-test-cicd-${var.eu_west_1}"
-  acl    = "private"
   provider = aws.eu_west_1
 }
 
 resource "aws_s3_bucket" "prod-cicd-eu-west-1" {
   bucket = "${var.org}-prod-cicd-${var.eu_west_1}"
-  acl    = "private"
   provider = aws.eu_west_1
 }
