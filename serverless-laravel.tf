6aa2776dbe430702d175c45ab3e101c3ab5020a3
Rizart Dokollari <r.dokollari@gmail.com>
2021-06-23 21:10:37 +0100

    Remove secrets authorisation.

--- serverless-laravel.tf
+++ serverless-laravel.tf
@@ -38,18 +38,14 @@ resource "aws_iam_user_policy_attachment" "serverless-laravel-global" {
 data "aws_iam_policy_document" "serverless-laravel" {
   statement {
     sid = replace(var.serverless-laravel-cicd-name, "-", "")
-    actions = concat(
-      var.cloudformation_actions, var.iam_sam_actions, var.lambda_actions, var.s3_cicd_actions,
-      ["secretsmanager:GetSecretValue"]
-    )
+    actions = concat( var.cloudformation_actions, var.iam_sam_actions, var.lambda_actions, var.s3_cicd_actions )
     resources = [
       "arn:aws:cloudformation:${var.eu_west_1}:${var.aws_account_id}:stack/${var.org}-*-${var.serverless-laravel}/*",
       "arn:aws:cloudformation:${var.eu_west_1}:aws:transform/Serverless-2016-10-31",
       "arn:aws:s3:::${aws_s3_bucket.prod-cicd-eu-west-1.bucket}/*",
       "arn:aws:s3:::${aws_s3_bucket.test-cicd-eu-west-1.bucket}/*",
       "arn:aws:iam::${var.aws_account_id}:role/${var.org}-*-${var.serverless-laravel}",
-      "arn:aws:lambda:${var.eu_west_1}:${var.aws_account_id}:function:${var.org}-*-${var.serverless-laravel}",
-      "arn:aws:secretsmanager:${var.eu_west_1}:${var.aws_account_id}:secret:${var.org}-*-${var.serverless-laravel}",
+      "arn:aws:lambda:${var.eu_west_1}:${var.aws_account_id}:function:${var.org}-*-${var.serverless-laravel}"
     ]
   }
 }
