c7a9bd2f5f08b8df59582b09c6dcbb638ad78d0a
Rizart Dokollari <r.dokollari@gmail.com>
2021-06-26 06:15:00 +0100

    Limit assume role policy.

--- user.tf
+++ user.tf
@@ -6,12 +6,12 @@ resource "aws_iam_user" "cicd-os" {
   }
 }
 
-data "aws_iam_policy_document" "instance-assume-role-policy" {
+data "aws_iam_policy_document" "cicd-os-assume-role-policy" {
   statement {
     actions = ["sts:AssumeRole"]
     principals {
-      type        = "Service"
-      identifiers = ["ec2.amazonaws.com"]
+      type        = "AWS"
+      identifiers = [aws_iam_user.cicd-os.arn]
     }
   }
 }
