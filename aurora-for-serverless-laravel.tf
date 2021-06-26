4c98085957c8c573f8c0e50615913417af2a7489
Rizart Dokollari <r.dokollari@gmail.com>
2021-06-26 07:03:14 +0100

    Format.

--- aurora-for-serverless-laravel.tf
+++ aurora-for-serverless-laravel.tf
@@ -12,7 +12,7 @@ resource "aws_iam_role" "aurora-for-serverless-laravel" {
   name               = var.aurora-for-serverless-laravel
   tags               = { Name = var.aurora-for-serverless-laravel }
   path               = "/${var.aurora-for-serverless-laravel}/"
-  assume_role_policy = data.aws_iam_policy_document.cicd-os-assume-role-policy.json
+  assume_role_policy = data.aws_iam_policy_document.cicd-allow--trusted-identifiers.json
 }
 
 data "aws_iam_policy_document" "aurora-for-serverless-laravel-global" {
